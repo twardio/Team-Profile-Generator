317a46e5f289b8099714d7fa8de3c336b2d0bf9e
Tom <thomas.f.ward.89@gmail.com>
2023-02-24 18:47:37 +0000

    starter code from bootcamp added

---
+++ employee.js
@@ -0,0 +1,10 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) { // or whatever parameters are required
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
+
+module.exports = Employee;
